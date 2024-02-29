f6ee083739bfdde10a4529ecf456b88ae4e91326
Rojan Gharibpour <sojaner@gmail.com>
2024-02-29 12:30:33 +0100

    Code sample

--- codegen.ts
+++ codegen.ts
@@ -1,10 +1,13 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
+import { addTypenameSelectionDocumentTransform } from "@graphql-codegen/client-preset";
+
+const addTypenames = process.argv.includes("--add-typenames");
 
 const config: CodegenConfig = {
   schema: "schema.graphql",
   documents: "document.graphql",
   generates: {
-    "types.ts": { plugins: ["typescript", "typescript-operations"] },
+    "types.ts": { plugins: ["typescript", "typescript-operations"], documentTransforms: [ ...(addTypenames ? [addTypenameSelectionDocumentTransform] : [])] },
   },
 };
 
--- types.ts
+++ types.ts
@@ -3,33 +3,44 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createRegion?: Maybe<Region>;
+};
+
+
+export type MutationCreateRegionArgs = {
+  regionDescription: Scalars['String']['input'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  user: User;
+  regions?: Maybe<Array<Maybe<Region>>>;
 };
 
-
-export type QueryUserArgs = {
-  id: Scalars['ID'];
+export type Region = {
+  __typename?: 'Region';
+  regionDescription: Scalars['String']['output'];
+  regionId: Scalars['Int']['output'];
 };
 
-export type User = {
-  __typename?: 'User';
-  email: Scalars['String'];
-  id: Scalars['ID'];
-  username: Scalars['String'];
+export type Subscription = {
+  __typename?: 'Subscription';
+  onRegionCreated: Region;
 };
 
-export type UserQueryVariables = Exact<{ [key: string]: never; }>;
+export type OnRegionCreatedSubscriptionVariables = Exact<{ [key: string]: never; }>;
 
 
-export type UserQuery = { __typename?: 'Query', user: { __typename?: 'User', id: string, username: string, email: string } };
+export type OnRegionCreatedSubscription = { __typename?: 'Subscription', onRegionCreated: { __typename?: 'Region', regionId: number, regionDescription: string } };
